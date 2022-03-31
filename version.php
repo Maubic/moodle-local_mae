4d5dbd0eb761ef3722a63515f76d64e6c9f19e53
Guillermo <glaseca@maubic.es>
2022-03-31 10:49:21 +0200

    Update version.php

--- version.php
+++ version.php
@@ -10,7 +10,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2022032100;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2022033100;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2020061504;        // Requires this Moodle version
 $plugin->supported = [38,39];
 // $plugin->incompatible = 310;
